e7db7534c63b388e34a6eb3c2857d86a65cac578
Endacott <tom_endacott@ajg.com>
2023-01-24 17:29:01 +0000

    Add second graphy, updated data, new css

--- app.py
+++ app.py
@@ -6,30 +6,59 @@ app = Dash(__name__)
 
 df = pd.read_csv('dataset-all.csv')
 
-fig = px.scatter(df,
+# ENTRY NUMBERS
+entrants_columns = df.columns[1:10]
+entry_number_fig = px.scatter(df,
                  x=df['Year'],
-                 y=df.columns[1:10])
-fig.update_traces(mode='markers+lines')
-fig.update_xaxes(tickmode='linear',
+                 y=entrants_columns)
+entry_number_fig.update_traces(mode='markers+lines')
+entry_number_fig.update_xaxes(tickmode='linear',
                  tickangle=-45,
                  rangeslider_visible=True)
-fig.update_yaxes(tickmode='linear',
+entry_number_fig.update_yaxes(tickmode='linear',
                  tick0=50,
                  dtick=50,
                  minor=dict(tick0=25, dtick=25, showgrid=True))
-fig.update_layout(
+entry_number_fig.update_layout(
     xaxis_title='Year',
     yaxis_title='Entries',
-    legend_title_text='Click to add or remove categories',
+    legend_title_text='Category',
+    legend=dict(
+        orientation='h'
+    )
+)
+
+# FINISHERS NUMBERS
+finisher_percent_columns = df.columns[19:28]
+finisher_percent_fig = px.scatter(df,
+                                  x=df['Year'],
+                                  y=finisher_percent_columns)
+finisher_percent_fig.update_traces(mode='markers+lines')
+finisher_percent_fig.update_xaxes(tickmode='linear',
+                                  tickangle=-45,
+                                  rangeslider_visible=True)
+finisher_percent_fig.update_yaxes(tickmode='linear',
+                                  tick0=10,
+                                  dtick=10,
+                                  minor=dict(tick0=5, dtick=5, showgrid=True))
+finisher_percent_fig.update_layout(
+    xaxis_title='Year',
+    yaxis_title='Finisher Percentage',
+    legend_title_text='Category',
     legend=dict(
         orientation='h'
     )
 )
 
 app.layout = html.Div([
-    html.H1('Dakar Entry Numbers'),
-    dcc.Graph(figure=fig,
-              id='entry-number-graph')
+    html.H1('Dakar Stats'),
+    html.P('Single click to add or remove a category. Double click to show only that category or show all categories.'),
+    html.H2('Entry Numbers'),
+    dcc.Graph(figure=entry_number_fig,
+              id='entry-number-graph data-graph'),
+    html.H2('Finisher Percentage'),
+    dcc.Graph(figure=finisher_percent_fig,
+              id='finisher-number-graph data-graph')
 ])
 
 app.run_server(debug=True)
