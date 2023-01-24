4cf332104e178a8f496dd10beb824af9df0d53d3
Endacott <tom_endacott@ajg.com>
2023-01-24 15:54:27 +0000

    Add app.py

--- app.py
+++ app.py
@@ -1,48 +1,35 @@
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html
 import plotly.express as px
 import pandas as pd
 
 app = Dash(__name__)
 
-df = pd.read_csv('dakar-stats-dataset.csv')
+df = pd.read_csv('dataset-all.csv')
+
+fig = px.scatter(df,
+                 x=df['Year'],
+                 y=df.columns[1:10])
+fig.update_traces(mode='markers+lines')
+fig.update_xaxes(tickmode='linear',
+                 tickangle=-45,
+                 rangeslider_visible=True)
+fig.update_yaxes(tickmode='linear',
+                 tick0=50,
+                 dtick=50,
+                 minor=dict(tick0=25, dtick=25, showgrid=True))
+fig.update_layout(
+    xaxis_title='Year',
+    yaxis_title='Entries',
+    legend_title_text='Click to add or remove categories',
+    legend=dict(
+        orientation='h'
+    )
+)
 
 app.layout = html.Div([
-    html.H1('Dakar Entry Numbers',
-            style={'textAlign': 'center', 'margin': '0', 'font-family': 'sans-serif'}),
-    dcc.Graph(id='my-graph',
-              style={'height': '90vh'}),
-    dcc.Checklist(
-        id='my-checklist',
-        options=df['category'].unique(),
-        value=df['category'].unique(),
-        style={'textAlign': 'center', 'font-family': 'sans-serif'}
-    )
+    html.H1('Dakar Entry Numbers'),
+    dcc.Graph(figure=fig,
+              id='entry-number-graph')
 ])
 
-
-@app.callback(
-    Output('my-graph', 'figure'),
-    Input('my-checklist', 'value')
-)
-def update_graph(selected_categories):
-    mask = df['category'].isin(selected_categories)
-    fig = px.scatter(df[mask],
-                     x='year',
-                     y='entries',
-                     color='category')
-    fig.update_traces(mode='markers+lines')
-    fig.update_xaxes(tickmode='linear',
-                     tickangle=-45)
-    fig.update_yaxes(tickmode='linear',
-                     tick0=50,
-                     dtick=50,
-                     minor=dict(tick0=25, dtick=25, showgrid=True))
-    fig.update_layout(
-        xaxis_title="Year",
-        yaxis_title="Entries"
-    )
-    return fig
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+app.run_server(debug=True)
